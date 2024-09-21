28f2c8267511b84e5db0e0266e0f9279def91785
handbob <handbob@icloud.com>
2024-09-21 19:49:02 +0200

    docs: Add comments to DataModule for better understanding

--- src/data/data.module.ts
+++ src/data/data.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { DataService } from './data.service'
 import { Customer } from './entities/customer.entity'
 
+// DataModule handles data-related operations and providers
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer])],
-  providers: [DataService],
-  exports: [DataService],
+  imports: [
+    // Register the Customer entity with TypeORM
+    TypeOrmModule.forFeature([Customer]),
+  ],
+  providers: [DataService], // Register DataService as a provider
+  exports: [DataService], // Export DataService for use in other modules
 })
 export class DataModule {}
